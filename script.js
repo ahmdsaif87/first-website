cc89192fd47010b8259f8f2bf6058665654beb8f
ahmdsaif87 <ahmadsaif7456@gmail.com>
2024-11-13 22:49:33 +0700

    okok

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
     const toggleButton = document.querySelector('.btn-toggle');
     const navbar = document.querySelector('.navbar');
+    const btnSubmit = document.getElementById('btnSubmit');
 
-    toggleButton.addEventListener('click', () => {
-        navbar.classList.toggle('active');
-    });
+    // Event listener untuk tombol toggle navbar
+    if (toggleButton && navbar) {
+        toggleButton.addEventListener('click', () => {
+            navbar.classList.toggle('active');
+        });
+    }
+
+    // Event listener untuk tombol submit
+    if (btnSubmit) {
+        btnSubmit.addEventListener('click', (event) => {
+            event.preventDefault(); 
+            Swal.fire({
+                icon: "error",
+                title: "Sorry...",
+                text: "this feature is not available yet",
+            });
+        });
+    }
 });
